4c561d14199ba63ee5e885ce9ca121f6ecb4ba7a
Carla Estrada <carla.estrada@orium.com>
2024-07-11 13:17:57 -0700

    try dynamic import makeswift page

--- app/[[...path]]/page.tsx
+++ app/[[...path]]/page.tsx
@@ -1,11 +1,16 @@
+import dynamic from 'next/dynamic'
 import { notFound } from 'next/navigation'
 
-import { Page as MakeswiftPage } from '@makeswift/runtime/next'
+//import { Page as MakeswiftPage } from '@makeswift/runtime/next'
 import { getSiteVersion } from '@makeswift/runtime/next/server'
 
 import { client } from '@/lib/makeswift/client'
 
 type ParsedUrlQuery = { path?: string[] }
+
+const MakeswiftPage = dynamic(() => import('@makeswift/runtime/next').then(mod => mod.Page), {
+  ssr: false,
+})
 
 export async function generateStaticParams() {
   return await client
